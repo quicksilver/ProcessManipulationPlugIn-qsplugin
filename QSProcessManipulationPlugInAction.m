a8428fccdc38ed4141d3f20892bd7bbfe9cebcf1
Jon Stovell <jonstovell@gmail.com>
2016-03-08 16:26:10 -0700

    ... Make that SIGTERM rather than SIGKILL
    Actually, OS X's Force Quit sends SIGTERM. Either would work, really, but for the sake of adhering as closely as possible to OS X's standards, we may as well use SIGTERM.

--- QSProcessManipulationPlugInAction.m
+++ QSProcessManipulationPlugInAction.m
@@ -88,7 +88,7 @@ return nil;
 }
 
 - (QSObject *) killProcess:(QSObject *)dObject{
-	[self sendSignal:SIGKILL toProcess:dObject];
+	[self sendSignal:SIGTERM toProcess:dObject];
 	return nil;
 }
 - (QSObject *) suspendProcess:(QSObject *)dObject{
